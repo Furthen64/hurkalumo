6a742f6c919b61e43628a5fed4fb35dc0420c014
Furthen64 <35999444+Furthen64@users.noreply.github.com>
2018-01-31 22:57:37 +0100

    Alpha stuff
    Trying to get Grid to work

--- src/Hurka/src/Core.cpp
+++ src/Hurka/src/Core.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <math.h>
 #include <SFML/Graphics.hpp>
 #include <chrono>
 
@@ -13,6 +14,150 @@ using namespace sf;
 /// Globals
 
 
+
+
+
+/// //////////////////////////////////////////////
+/// Grid on the ground
+
+class Grid
+{
+public:
+    Grid(void)
+    {
+        texture.loadFromFile("C:\\github\\lumo\\src\\Hurka\\bin\\Release\\GRID.png");
+        sprite = Sprite(texture);
+    }
+
+
+    void draw( RenderTarget& rt)
+    {
+        // haha this will be hell to figure out
+        // I need functions for getting the X,Y of the [N,M]th position in the matrix
+
+
+        for(int y = 0; y < 600; y+=25)
+        {
+            float yF = y;
+            sprite.setPosition({0,yF});
+            rt.draw(sprite);
+
+
+        }
+
+    }
+
+
+private:
+    Texture texture;
+    Sprite sprite;
+};
+
+
+
+/// //////////////////////////////////////////////
+/// and up comes the Toolbar
+
+class Toolbar
+{
+public:
+    Toolbar(const Vector2f& _pos)
+        :
+        pos(_pos)
+    {
+
+        toolbarXOffset = pos.x;
+
+
+        // get the whole texture for all the buttons
+        texture.loadFromFile("C:\\github\\lumo\\src\\Hurka\\bin\\Release\\TOOLBAR.png");
+
+
+
+        // allocate all the sprites
+        sprites = new Sprite[nrButtons*2];
+        visibleSpritesTopArr = new int [nrButtons];
+        for(int i = 0; i < nrButtons; i++) { visibleSpritesTopArr[i] = true; }
+
+
+        // map all the sprites correctly
+        // position the sprites
+        float x, y;
+
+        for(int i = 0; i < nrButtons*2; i++){
+            sprites[i] = Sprite(texture);
+            sprites[i].setTextureRect( {i*widthPx, 0, 16, 16} );
+
+            x = i*widthPx+ toolbarXOffset;
+            y = 0;
+
+            sprites[i].setPosition({x,y});
+        }
+
+    }
+
+    ~Toolbar()
+    {
+        delete sprites;
+        delete visibleSpritesTopArr;
+    }
+
+    void pushButton(int relXPos)
+    {
+        // set the status of the dang thing
+        visibleSpritesTopArr[0] = !visibleSpritesTopArr[0];
+
+    }
+
+    void draw( RenderTarget& rt)
+    {
+        // draw all the buttons individually
+        for(int i = 0; i < nrButtons; i++){
+            if(visibleSpritesTopArr[i] == true) {
+                sprites[i].setTextureRect( {i*widthPx, 0, 16, 16} );    // Show the top one
+            } else {
+                sprites[i].setTextureRect( {i*widthPx, 16, 16, 16} );
+            }
+
+            // Draw it
+            rt.draw( sprites[i] );
+        }
+    }
+
+
+
+
+    Vector2f getPos()
+    {
+        return pos;
+    }
+
+    int *getVisibleSpritesTopArr()
+    {
+        return visibleSpritesTopArr;
+    }
+
+private:
+    Vector2f pos;
+
+    Texture texture;    // we need one texture but
+    Sprite *sprites;      // we need 12 sprites for each of the 6 buttons
+
+    int toolbarXOffset = 240;
+
+    int *visibleSpritesTopArr;  // Top array that controls which are visible
+                                // 1 = show the top one (unpressed)
+                                // 0 = show the one below (pressed)
+
+
+
+
+
+    const int nrButtons = 6;
+    const int widthPx = 16;
+};
+
+
 /// //////////////////////////////////////////////
 /// Locomotive
 class Locomotive
@@ -22,14 +167,11 @@ public:
         :
         pos(_pos)
     {
-        // ? ?
-        texture.loadFromFile("C:\\fat64\\projects\\Hurka\\bin\\Release\\ANIM_001.png");
+        texture.loadFromFile("C:\\github\\lumo\\src\\Hurka\\bin\\Release\\LOCOMOTIVE.png");
         sprite = Sprite(texture);
-        sprite.setTextureRect( {0,0,30,30});
-
-
-
+        sprite.setTextureRect( {0,0,32,32});
     }
+
     void draw( RenderTarget& rt) const
     {
         rt.draw( sprite );
@@ -71,10 +213,15 @@ private:
 int main()
 {
     // Setup Window
-    RenderWindow window(sf::VideoMode(640, 480), "Lumo Editor 0.1-alpha");
+    RenderWindow window(sf::VideoMode(800, 600), "Hurkalumo Editor 0.1-alpha", Style::Fullscreen);
+
+
+
 
     // Setup objects
     Locomotive loco({10.0f, 10.0f});
+    Toolbar toolbarTop({260.0f, 0.0f});
+    Grid grid;
 
     // Setup timing
     auto tp = std::chrono::steady_clock::now();
@@ -87,9 +234,6 @@ int main()
     while (window.isOpen())
     {
 
-
-
-
         /// Get events
 
         Event event;
@@ -101,6 +245,7 @@ int main()
 
 
 
+        // I dont understand this
         float dt;
         {
             const auto newTp = std::chrono::steady_clock::now();
@@ -119,6 +264,12 @@ int main()
             Vector2f mousePos_f = Vector2f();
             mousePos_f.x = mousePos_i.x;
             mousePos_f.y = mousePos_i.y;
+
+
+            /// Pushing button on top toolbar
+
+            // if (toolbarTop.within(mousePos_i.x, mousepPos_i.y) { pushbutton(x,y));
+            toolbarTop.pushButton(0); // debug test
 
             bool rightof = false;
             bool belowof = false;
@@ -161,16 +312,19 @@ int main()
         }
 
         loco.update(dt);
-
+        //toolbarTop.update(); TODO should I do this?
 
 
 
 
         /// Render
-        window.clear();
+        window.clear({198, 198, 198});
 
         // Draw sprites
         loco.draw(window);
+        toolbarTop.draw(window);
+        grid.draw(window);
+
 
         window.display();
 
